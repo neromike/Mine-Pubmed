5fb336745f552b4c29d7739b8fe3680985c72f60
Pereanu <github.20.bushky@spamgourmet.com>
2012-01-19 15:50:50 -0500

    refactoring interface, not complete yet

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <?php include "mine_pubmed.php"; ?>
 
 <?php
-$query_default = "(autism OR asd) AND EEG AND (NOT review)";
+$query_default = "autism AND dti";
 $date_from_default = "2008";
 $date_to_default = "2008";
 if (isset($_GET['query'])) { $query = $_GET['query']; } else { $query = "*"; }
@@ -17,40 +17,46 @@ if (isset($_GET['date_from_old'])) { $date_from_old = $_GET['date_from_old']; } 
 if (isset($_GET['date_to_old'])) { $date_to_old = $_GET['date_to_old']; } else { $date_to_old = $date_to_default; }
 if (isset($_GET['review'])) { $review = $_GET['review']; } else { $review = "off"; }
 
-if ($query == "*") {
-	#No query has been provided
-	echo '<form action="index.php" method="get">';
-		echo '';
-		echo "<br />";
-		echo '<input name="query" size="100" value="' . $query_old . '" />';
-		echo '<br />';
-		echo '<table>';
-		echo '<tr>';
-		echo '<td><input type="checkbox" name="date_from_check" /></td>';
-		echo '<td>Date from</td><td align="right">January 1st,</td><td><input type="date" name="date_from" value="' . $date_from_old . '"/></td>';
-		echo "</tr><tr>";
-		echo '<td><input type="checkbox" name="date_to_check" /></td>';
-		echo '<td>Date to</td><td align="right">December 31st,</td><td><input type="date" name="date_to" value="' . $date_to_old . '"/></td>';
-		echo '</table>';
-		echo '<table><tr><td><input type="checkbox" name="review" /></td><td>Check this to include reviews</td></tr></table>';
-		echo '<input type="submit" value="Query" />';
-	echo '</form>';	
-} else {
-	#Query has been provided
-	echo '<form action="index.php" method="get">';
-	echo '<input type="hidden" name="query_old" value="' . $query . '" />';
-	echo '<input type="hidden" name="date_from_old" value="' . $date_from . '" />';
-	echo '<input type="submit" value="New Query" />';
-	echo '</form>';
-	
-	if ($review == "off") { $query = $query + " AND (NOT review)"; }
-	echo "Query: " . $query;
-	
-	list($PMID, $count) = get_PMIDs($query);
 
+#The query form
+echo '<form action="' . basename($_SERVER['REQUEST_URI']) . '" method="get">';
+	
+	echo '<input type="hidden" name="date_from_old" value="' . $date_from . '" />';
+	echo '';
+	echo "<br />";
+	if ($query == "*") {
+		echo '<input name="query" size="100" value="' . $query_default . '" />';
+	} else {
+		echo '<input name="query" size="100" value="' . $query . '" />';
+	}
+	echo '<br />';
+	echo '<table>';
+	echo '<tr>';
+	echo '<td><input type="checkbox" name="date_from_check" /></td>';
+	echo '<td>Date from</td><td align="right">January 1st,</td><td><input type="date" name="date_from" value="' . $date_from_old . '"/></td>';
+	echo "</tr><tr>";
+	echo '<td><input type="checkbox" name="date_to_check" /></td>';
+	echo '<td>Date to</td><td align="right">December 31st,</td><td><input type="date" name="date_to" value="' . $date_to_old . '"/></td>';
+	echo '</table>';
+	echo '<table><tr><td><input type="checkbox" name="review" /></td><td>Check this to include reviews</td></tr></table>';
+	echo '<input type="submit" value="Query" />';
+echo '</form>';	
+
+
+
+if ($query != "*") {
+	#Query has been provided
+	
+	#if ($review == "off") { $query = $query + " AND (NOT review)"; }
+	#$query = $query + ' AND ("2011/12/01"[Date - Entrez] : "2011/12/31"[Date - Entrez]) ';
+	
+	$query = urlencode($query);
+	echo "Query: " . $query;
+	echo "<hr />";
+	#list($PMID, $count) = get_PMIDs($query);
 	echo $count . "<br>";
 	
-	list($authors, $title, $abstract, $date_pubmed, $date_rec, $date_acc, $journal, $affil) = get_summary($PMID[11]);
+	list($authors, $title, $abstract, $date_pubmed, $date_rec, $date_acc, $journal, $affil) = get_summary($PMID[2]);
 	
 	echo "<Strong>authors</strong>:";
 	print_r($authors);
