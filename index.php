99d61f66dd0f3b714e5b8613b8e360a77da1d8fa
neromike <github.20.bushky@spamgourmet.com>
2012-12-18 00:25:59 -0500

    Updated
    Updated to use XML output from Entrez and use jQuery's datepicker

--- index.php
+++ index.php
@@ -1,90 +1,108 @@
-<HTML>
-<HEAD>
-<TITLE>Mine_pubmed</TITLE>
-</HEAD>
-<BODY>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Mine_pubmed</title>
+<link rel="stylesheet" href="jquery-ui-1.9.2.custom.min.css" />
+<script type="text/javascript" src="jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="jquery-ui-1.9.2.custom.min.js"></script>
+</head>
+<script>$(function() { $( ".datepicker" ).datepicker( {dateFormat: 'yy/mm/dd'} ); });</script>
+<script>
+function restrict_date(cb) {
+	if (cb.checked) {
+		document.getElementById("restrict_date_range").style.display = "inherit";
+	} else {
+		document.getElementById("restrict_date_range").style.display = "none";
+	}
+}
+</script>
+<body>
 
 <?php include "mine_pubmed.php"; ?>
 
 <?php
 $query_default = "(autism OR autistic) AND dti";
-$date_from_default = "1966";
+$date_from_default = "2006/01/01";
 $date_from_check_default = "";
 $date_to_check_default = "";
-$date_to_default = "2011";
+$date_to_default = "2012/12/31";
 if (isset($_GET['query'])) { $query = $_GET['query']; } else { $query = "*"; }
-if (isset($_GET['date_from_check'])) { $date_from_check = $_GET['date_from_check']; } else { $date_from_check = $date_from_check_default; }
-if (isset($_GET['date_to_check'])) { $date_to_check = $_GET['date_to_check']; } else { $date_to_check = $date_to_check_default; }
+if (isset($_GET['date_restrict'])) { $date_restrict = $_GET['date_restrict']; } else { $date_restrict = $date_from_check_default; }
+	if ($date_restrict != "on") { echo "<style>#restrict_date_range { display:none; }</style>"; }
 if (isset($_GET['date_from'])) { $date_from = $_GET['date_from']; } else { $date_from = $date_from_default; }
 if (isset($_GET['date_to'])) { $date_to = $_GET['date_to']; } else { $date_to = $date_to_default; }
-if (isset($_GET['review'])) { $review = $_GET['review']; } else { $review = "off"; }
+#if (isset($_GET['review'])) { $review = $_GET['review']; } else { $review = "off"; }
 
 
 #The query form
 echo '<form action="index.php" method="get">';
-	echo '<input type="hidden" name="date_from_old" value="' . $date_from . '" />';
-	if ($query == "*") {
-		echo '<input name="query" size="100" value="' . $query_default . '" />';
-	} else {
-		echo '<input name="query" size="100" value="' . $query . '" />';
-	}
+
+	#query
+	echo '<input name="query" size="100" value="';
+	if ($query == "*") { echo $query_default; } else { echo $query; }
+	echo '" />';
 	echo '<table>';
 	echo '<tr>';
 	
-	echo '<td><input type="checkbox" name="date_from_check" ';
-	if ($date_from_check == "on") { echo " checked='yes'"; }
-	echo '/></td>';
-	
-	echo '<td>Date from</td><td align="right">January 1st,</td><td><input type="date" name="date_from" value="' . $date_from . '"/></td>';
+	#restricted date
+	echo '<td>Restrict date?<input type="checkbox" name="date_restrict"';
+	if ($date_restrict == "on") { echo " checked='yes' "; }
+	echo ' onClick="restrict_date(this)" /></td>';
+	echo '<td id="restrict_date_range">From <input class="datepicker" name="date_from" value="' . $date_from . '"/> to <input class="datepicker" name="date_to" value="' . $date_to . '"/></td>';
 	echo "</tr><tr>";
 	
-	echo '<td><input type="checkbox" name="date_to_check"';
-	if ($date_to_check == "on") { echo " checked='yes'"; }
-	echo '/></td>';
-	
-	echo '<td>Date to</td><td align="right">December 31st,</td><td><input type="date" name="date_to" value="' . $date_to . '"/></td>';
 	echo '</table>';
-	echo '<table><tr><td><input type="checkbox" name="review" /></td><td>Check this to include reviews</td></tr></table>';
+	
+	#reviews
+	#echo '<table><tr><td><input type="checkbox" name="review" /></td><td>Check this to include reviews</td></tr></table>';
+	
+	#submit
 	echo '<input type="submit" value="Query" />';
+	
 echo '</form>';	
 
 
 
-if ($query != "*") {
-	#Query has been provided
+if ($query != "*") { #Query has been provided
+	#if ($review == "off") { $query = $query . " AND (NOT review)"; }
+
 	
-	if ($review == "off") { $query = $query . " AND (NOT review)"; }
-	$query = $query . ' AND ("2011/12/01"[Date - Entrez] : "2011/12/31"[Date - Entrez]) ';
+	if ($date_restrict == "on") {
+		$query = $query . ' AND ("' . $date_from . '"[Date - Entrez] : "' . $date_to . '"[Date - Entrez]) ';
+	}
 	
+	echo "Query: " . $query;
+	echo "<br />";
 	$query = urlencode($query);
 	echo "Query: " . $query;
 	echo "<hr />";
 	list($PMID, $count) = get_PMIDs($query);
 	echo $count . "<br>";
 	
-	list($authors, $title, $abstract, $date_pubmed, $date_rec, $date_acc, $journal, $affil) = get_summary($PMID[2]);
-	
-	echo "<Strong>authors</strong>:";
-	print_r($authors);
-	echo "<br />";
-	echo "<strong>title</strong>:" . $title . "<br>";
-	echo "<strong>abstract</strong>:" . $abstract . "<br>";
-	echo "<strong>date (pubmed):</strong>";
-	print_r($date_pubmed);
-	echo "<br />";
-	echo "<strong>date (received):</strong>";
-	print_r($date_rec);
-	echo "<br />";
-	echo "<strong>date (accepted):</strong>";
-	print_r($date_acc);
-	echo "<br />";
-	echo "<strong>journal:</strong>" . $journal . "<br />";
-	echo "<strong>affiliation:</strong>" . $affil . "<br />";
-	echo "-------------------------------<br />";
-
+	if (1==1) {
+		list($authors, $title, $abstract, $date_pubmed, $date_rec, $date_acc, $journal, $affil) = get_summary($PMID[3]);
+		echo "<strong>authors</strong>:";
+		print_r($authors);
+		echo "<br />";
+		echo "<strong>title</strong>:" . $title . "<br>";
+		echo "<strong>abstract</strong>:" . $abstract . "<br>";
+		echo "<strong>date (pubmed):</strong>";
+		print_r($date_pubmed);
+		echo "<br />";
+		echo "<strong>date (received):</strong>";
+		print_r($date_rec);
+		echo "<br />";
+		echo "<strong>date (accepted):</strong>";
+		print_r($date_acc);
+		echo "<br />";
+		echo "<strong>journal:</strong>" . $journal . "<br />";
+		echo "<strong>affiliation:</strong>" . $affil . "<br />";
+		echo "-------------------------------<br />";
+	}
 }
 
 ?>
 
-</BODY>
-</HTML>
+</body>
+</html>
--- mine_pubmed.php
+++ mine_pubmed.php
@@ -4,7 +4,7 @@ function get_PMIDs ($query, $return_count=false) {
 	#output: returns an array with all the PMIDs
 	#echo "<strong>Query</strong>: " . $query . "<br />";
 	#second parameter is an optional boolean. If true, the function will just return the total count.
-	$base_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi';
+	$base_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed';
 	$done = False;
 	$ret_start = 0;
 	$ret_max = 100;
@@ -12,7 +12,7 @@ function get_PMIDs ($query, $return_count=false) {
 	$PMID = array();
 	$debug = False;
 	while (! $done) {
-		$query_ID = $base_URL . '?db=pubmed&term=' . $query . '&retmax=' . $ret_max . '&retStart=' . $ret_start;
+		$query_ID = $base_URL . '&term=' . $query . '&retmax=' . $ret_max . '&retStart=' . $ret_start;
 		$xml = simplexml_load_file( $query_ID );
 		if ($count == -1) {	#get the total number of matching publications
 			$count = $xml->Count;
@@ -47,124 +47,47 @@ function get_PMIDs ($query, $return_count=false) {
 }
 function get_summary ($this_PMID) {
 	#input: a single PMID
-	#output: an array of the pertinent information	
+	#output: an array of the pertinent information
+	$debug = True;
 	$base_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi';
-	$query_ID = $base_URL . '?db=pubmed&id=' . $this_PMID;
-	#echo '<a href="' . $query_ID . '" target="blank">' . $query_ID . '</a><br />';
+	$query_ID = $base_URL . '?db=pubmed&id=' . $this_PMID . "&retmode=xml";
+	if ($debug) {
+		echo '<a href="' . $query_ID . '" target="blank">' . $query_ID . '</a><br />';
+	}
 	$xml = simplexml_load_file( $query_ID );
-	$info = $xml->body->pre;
-	$info = str_replace("\t","", $info);
-	#echo $info . "<br><br>";
 	
 	#Get author names
-	$start = strpos($info, "names ml") + 21;
-	if ($start) {
-		$len = strpos(substr($info, $start), "}");
-		$authors = explode(",", substr($info, $start, $len));
-	} else {
-		$authors = "Not provided";
+	$authors = "";
+	foreach ($xml->PubmedArticle->MedlineCitation->Article->AuthorList->children() as $child) {
+		$authors = $authors . $child->LastName . " " . $child->Initials . ", ";
 	}
 	
-	#Get the title
-	$start = strpos($info, "cit {");
-	if ($start) {
-		$start = $start + strpos( substr($info, $start), '"') + 1;
-		$len = strpos(substr($info, $start), '"');
-		$title = substr($info, $start, $len);
-	} else {
-		$title = "Not provided";
-	}
-
-	#Get the abstract
-	$start = strpos($info, "abstract");
-	if ($start) {
-		$start = $start + strpos( substr($info, $start), '"') + 1;
-		$len = strpos(substr($info, $start), '"');
-		$abstract = substr($info, $start, $len);
-	} else {
-		$abstract = "Not provided";
+	#Get title
+	$title = $xml->PubmedArticle->MedlineCitation->Article->ArticleTitle;
+	
+	#Get abstract
+	$abstract = $xml->PubmedArticle->MedlineCitation->Article->Abstract->AbstractText;
+	
+	#Get dates
+	$date_pubmed = $xml->PubmedArticle->PubmedData->History->PubMedPubDate;
+	$date_rec = "";
+	$date_pubmed = "";
+	$date_acc = "";
+	foreach ($xml->PubmedArticle->PubmedData->History->children() as $child) {
+		if ($child['PubStatus'] == "received") {
+			$date_rec = array( $child->Year, $child->Month, $child->Day );
+		} elseif ($child['PubStatus'] == "pubmed") {
+			$date_pubmed = array( $child->Year, $child->Month, $child->Day );
+		} elseif ($child['PubStatus'] == "accepted") {
+			$date_acc = array( $child->Year, $child->Month, $child->Day );
+		}
 	}
 	
-	#Get the pubmed date
-	$start = strpos($info, "pubstatus pubmed");
-	if ($start) {
-		#get the year
-		$start = $start + strpos( substr($info, $start), 'year') + 5;
-		$len = strpos(substr($info, $start), ",");
-		$year = substr($info, $start, $len);
-		#get the month
-		$start = $start + strpos( substr($info, $start), 'month') + 6;
-		$len = strpos(substr($info, $start), ",");
-		$month = substr($info, $start, $len);
-		#get the day
-		$start = $start + strpos( substr($info, $start), 'day') + 4;
-		$len = strpos(substr($info, $start), "}");
-		$day = substr($info, $start, $len);
-		$date_pubmed = array($year, $month, $day);
-	} else {
-		$date_pubmed = "";
-	}
-	
-	#Get the received date
-	$start = strpos($info, "pubstatus received");
-	if ($start) {
-		#get the year
-		$start = $start + strpos( substr($info, $start), 'year') + 5;
-		$len = strpos(substr($info, $start), ",");
-		$year = substr($info, $start, $len);
-		#get the month
-		$start = $start + strpos( substr($info, $start), 'month') + 6;
-		$len = strpos(substr($info, $start), ",");
-		$month = substr($info, $start, $len);
-		#get the day
-		$start = $start + strpos( substr($info, $start), 'day') + 4;
-		$len = strpos(substr($info, $start), "}");
-		$day = substr($info, $start, $len);
-		$date_rec = array($year, $month, $day);
-	} else {
-		$date_rec = "";
-	}
-	
-	#Get the accepted date
-	$start = strpos($info, "pubstatus accepted");
-	if ($start) {
-		#get the year
-		$start = $start + strpos( substr($info, $start), 'year') + 5;
-		$len = strpos(substr($info, $start), ",");
-		$year = substr($info, $start, $len);
-		#get the month
-		$start = $start + strpos( substr($info, $start), 'month') + 6;
-		$len = strpos(substr($info, $start), ",");
-		$month = substr($info, $start, $len);
-		#get the day
-		$start = $start + strpos( substr($info, $start), 'day') + 4;
-		$len = strpos(substr($info, $start), "}");
-		$day = substr($info, $start, $len);
-		$date_acc = array($year, $month, $day);
-	} else {
-		$date_acc = "";
-	}
-	
-	#Get the journal
-	$start = strpos($info, "journal");
-	if ($start) {
-		$start = $start + strpos( substr($info, $start), "name");
-		$start = $start + strpos( substr($info, $start), '"') + 1;
-		$len = strpos( substr($info, $start), '"');
-		$journal = substr($info, $start, $len);
-	} else {
-		$journal = "Not provided";
-	}
+	#Get journal
+	$journal = $xml->PubmedArticle->MedlineCitation->Article->Journal->Title;
 	
 	#Get the affiliation
-	$start = strpos($info, "affil str");
-	if ($start) {
-		$start = $start + strpos( substr($info, $start), '"') + 1;
-		$len = strpos( substr($info, $start), '"');
-		$affil = substr($info, $start, $len);
-	} else {
-		$affil = "Not provided";
-	}
+	$affil = $xml->PubmedArticle->MedlineCitation->Article->Affiliation;
 	
 	return array($authors, $title, $abstract, $date_pubmed, $date_rec, $date_acc, $journal, $affil);
 }
